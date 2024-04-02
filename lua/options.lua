3607e6729e5f153df431f5291bbae52028d84b75
Erlend <erlenduh@gmail.com>
2024-04-02 14:45:47 +0200

    Initial commit

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+-- Leaderkey
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Numbers
+vim.opt.number = true
+
+-- Lines between cursor and top/bottom
+vim.opt.scrolloff = 10
+
+-- Disable nerd font
+vim.g.have_nerd_font = false
+
+-- Don't show mode
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim
+vim.opt.clipboard = 'unnamedplus'
