ccdde71c2bec94b818898318763547c01f16a885
Erlend <erlenduh@localhost.localdomain>
2024-04-18 03:21:55 +0200

    made changes

--- lua/options.lua
+++ lua/options.lua
@@ -3,13 +3,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Numbers
-vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Lines between cursor and top/bottom
 vim.opt.scrolloff = 10
-
--- Disable nerd font
--- vim.g.have_nerd_font = false
 
 -- Don't show mode
 vim.opt.showmode = false
--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,10 +1,15 @@
 return {
     {
-    "ellisonleao/gruvbox.nvim",
+    'folke/tokyonight.nvim',
+    lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme gruvbox")
+      vim.cmd('colorscheme tokyonight-moon')
     end
+    },
+
+    {
+    'neovim/nvim-lspconfig'
     },
 
     {
@@ -13,16 +18,27 @@ return {
     },
 
     {
-    "williamboman/mason.nvim"
+    'williamboman/mason.nvim',
+    "williamboman/mason-lspconfig.nvim",
     },
 
     {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-nvim-lsp',
+        },
+    },
+
+    {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
       }
     },
@@ -31,11 +47,36 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-            require("lualine").setup({
+            require('lualine').setup({
                 icons_enabled = true,
-                theme = 'onedark',
+                theme = 'tokyonight',
             })
         end,
     },
 
+    {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    },
+
+    {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+
+    {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    },
+
+    {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+    },
 }
